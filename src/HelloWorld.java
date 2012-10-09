b2d189c54e11c2bb686efa3f4fad16da555e5e6e
tmjohari <thaqif_mohdjohari@carleton.ca>
2012-10-09 10:37:44 -0300

    Update src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,11 @@
-
 public class HelloWorld {
+	//return string hello
 	public String hello(){
 		return "Hello";
 	}
+	//return string goodbye
 	public String goodBye(){
 		return "Goodbye";
 	}
+
 }
